812b9d6d30bdf3ba7d4ac8fa14a47c5f0c6923f3
tareq <tareq.elshrif@gmail.com>
2024-03-01 02:50:58 +0300

    create db + details for db for tower,appartment...etc

--- tower/admin.py
+++ tower/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import Employees,Tower,Apartments,Tenants
 # Register your models here.
+admin.site.register(Employees)
+admin.site.register(Tower)
+admin.site.register(Apartments)
+admin.site.register(Tenants)
--- tower/models.py
+++ tower/models.py
@@ -1,3 +1,45 @@
 from django.db import models
 
 # Create your models here.
+class Tower(models.Model):
+    tower_num = models.CharField(max_length=50)
+    address_tower = models.CharField(max_length=255)
+    floors_num = models.CharField(max_length=10)
+    statues_tower = models.CharField(max_length=50)
+    value_tower = models.CharField(max_length=50)
+    picture_tower = models.ImageField(upload_to="tower_picture/",null=True,blank=True)
+    notes = models.CharField(max_length=300)
+    
+class Apartments(models.Model):
+    tower = models.ForeignKey(Tower,on_delete=models.CASCADE)
+    apartment_num = models.CharField(max_length=50)
+    floor=models.CharField(max_length=10)
+    space=models.CharField(max_length=30)
+    rooms_num=models.CharField(max_length=10)
+    rooms_service_num=models.CharField(max_length=10)
+    wc_num=models.CharField(max_length=10)
+    statues_apartment=models.CharField(max_length=50)
+    rent=models.CharField(max_length=10)
+    duration=models.CharField(max_length=10)
+    maintenance=models.CharField(max_length=10)
+    picture=models.ImageField(upload_to="apartments_picture/",null=True,blank=True)
+    notes=models.CharField(max_length=300)
+    
+class Tenants (models.Model):
+    apartments = models.ForeignKey(Apartments,on_delete=models.CASCADE)
+    name=models.CharField(max_length=80)
+    id_person=models.CharField(max_length=20)
+    nationality=models.CharField(max_length=20)
+    phone=models.CharField(max_length=20)
+    picture =models.ImageField (upload_to="tenants_picture/",null=True,blank=True)
+    notes=models.CharField(max_length=300)
+    
+class Employees (models.Model):
+    tower = models.ForeignKey(Tower,on_delete=models.CASCADE)
+    name=models.CharField(max_length=80)
+    id_person=models.CharField(max_length=20)
+    phone=models.CharField(max_length=20)
+    picture=models.ImageField (upload_to="employees_picture/",null=True,blank=True)
+    notes=models.CharField(max_length=300)
+
+
